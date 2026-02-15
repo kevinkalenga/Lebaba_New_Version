eaf69e1d00c67dc1b4d91cceb350403ab3ce9d33
kevin kalenga <kevinkalenga10@gmail.com>
2026-02-15 04:35:45 +0100

    Project Setup and Install Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+ 
+
+  return (
+  <div className='w-full'>
+    <h1 className="text-3xl font-bold underline bg-red-500 text-white text-center">
+      Hello world!
+    </h1>
+  </div>
+  )
+}
+
+export default App
