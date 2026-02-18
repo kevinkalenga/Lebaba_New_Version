1a730be59d89ec9b8195bfc18239108937ba7215
kevin kalenga <kevinkalenga10@gmail.com>
2026-02-18 10:09:06 +0100

    Customize your project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
   <div className='w-full'>
-    <h1 className="text-3xl font-bold underline bg-red-500 text-white text-center">
+    <h1 className="text-3xl font-bold underline bg-red-500 text-white text-center p-2">
       Hello world!
     </h1>
   </div>
