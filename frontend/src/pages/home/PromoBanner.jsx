3a578a8195c9a53be3f598b662a6fe5d4619e52b
kevin kalenga <kevinkalenga10@gmail.com>
2026-02-25 14:03:12 +0100

    Promotional Banner

---
+++ frontend/src/pages/home/PromoBanner.jsx
@@ -0,0 +1,32 @@
+
+
+const PromoBanner = () => {
+    return (
+        <section className="section__container banner__container">
+            <div className="banner__card">
+                <span>
+                    <i className="ri-truck-line"></i>
+                    <h4>Free Delivery</h4>
+                    <p>Offers convenience and the ability to shop from anywhere, anytime.</p>
+                </span>
+            </div>
+            <div className="banner__card">
+                <span>
+                    <i className="ri-money-dollar-circle-line"></i>
+                    <h4>100% Money Back Guaranty</h4>
+                    <p>E-commerce have a review systeme where customers can share feedback</p>
+                </span>
+            </div>
+            <div className="banner__card">
+                <span>
+                    <i className="ri-user-voice-fill"></i>
+                    <h4>Strong Support</h4>
+                    <p>Offer customer support services to assist customers
+                        with queries and issues</p>
+                </span>
+            </div>
+        </section>
+    )
+}
+
+export default PromoBanner
