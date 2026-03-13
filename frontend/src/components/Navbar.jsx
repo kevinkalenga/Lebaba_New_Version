903ea3a8f3a8a9ca95e85fd8cee1bc0264759659
kevin kalenga <kevinkalenga10@gmail.com>
2026-03-13 09:45:13 +0100

    Show Cart Modal

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
+import CartModal from '../pages/shop/CartModal'
 
 const Navbar = () => {
   
   const products = useSelector((state) => state.cart.products);
+  const [isCartOpen, setIsCartOpen] = useState(false)
+
+   const handleCartToggle = () => {
+        setIsCartOpen(!isCartOpen)
+    }
   
   
   return (
@@ -28,7 +34,7 @@ const Navbar = () => {
                 <Link to="/search"><i className="ri-search-line"></i></Link>
               </span>
               <span>
-                <button className='hover:text-primary'>
+                <button onClick={handleCartToggle} className='hover:text-primary'>
                    <i className="ri-shopping-bag-line"></i>
                     <sup className='text-sm inline-block px-1.5 text-white rounded-full  bg-primary text-center'>{products.length}</sup>
                 </button>
@@ -40,6 +46,10 @@ const Navbar = () => {
               </span>
           </div>
         </nav>
+
+        {
+          isCartOpen && <CartModal products={products} isOpen={isCartOpen} onClose={handleCartToggle}/>
+        }
     </header>
   )
 }
