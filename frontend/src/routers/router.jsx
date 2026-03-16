786cb8ed50ee7437f9f6c9515df9d4c856158ce5
kevin kalenga <kevinkalenga10@gmail.com>
2026-03-16 10:16:00 +0100

    Register Page

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -6,6 +6,7 @@ import Search from "../pages/search/Search";
 import ShopPage from '../pages/shop/ShopPage';
 import SingleProduct from '../pages/shop/productDetails/SingleProduct';
 import Login from '../components/Login';
+import Register from '../components/Register';
 
 const router = createBrowserRouter([
     {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
     {
         path: "/login",
         element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
     },
 ])
 
