45d15ad50314b86ae6b30d5bfe4e5e233ead2c0f
kevin kalenga <kevinkalenga10@gmail.com>
2026-04-07 12:34:52 +0200

    Create Reviews Schema and Model

---
+++ backend/src/reviews/reviews.model.js
@@ -0,0 +1,30 @@
+const { mongoose } = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+    comment: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+const Reviews = mongoose.model("Review", reviewSchema);
+
+module.exports = Reviews
