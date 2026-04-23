db0dcaccef106ca730095a615ac6a278a4ca10d0
kevin kalenga <kevinkalenga10@gmail.com>
2026-04-23 10:36:59 +0200

    Total Reviews count and Get Reviews by User Id

--- backend/src/reviews/reviews.route.js
+++ backend/src/reviews/reviews.route.js
@@ -58,6 +58,38 @@ router.post("/post-review", async (req, res) => {
     }
 });
 
+// total reviews count
+router.get("/total-review", async (req, res) => {
+    try {
+        const totalReviews = await Reviews.countDocuments({});
+        res.status(200).send({ totalReviews })
+    } catch (error) {
+        console.error("Error getting total review", error);
+        res.status(500).send({ message: "Failed to get review count" });
+    }
+})
+
+// get reviews by userid
+
+router.get("/:userId", async (req, res) => {
+    const { userId } = req.params;
+    if (!userId) {
+        return res.status(400).send({ message: "User ID is required" })
+    }
+
+    try {
+        const reviews = await Reviews.find({ userId: userId }).sort({ createdAt: -1 });
+        if(reviews.length === 0) {
+             return res.status(404).send({message: "No reviews found"});
+        }
+        return res.status(200).send(reviews);
+
+    } catch (error) {
+        console.error("Error fetching reviews by user", error);
+        return res.status(500).send({ message: "Failed to fetch reviews by user" });
+    }
+})
+
 
 
 
