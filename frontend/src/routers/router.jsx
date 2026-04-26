2b1fa0f058db6ed857a13f06b4c30ba30e808d7e
kevin kalenga <kevinkalenga10@gmail.com>
2026-04-26 14:04:41 +0200

    Setup Dashboard Routes

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -8,6 +8,7 @@ import SingleProduct from '../pages/shop/productDetails/SingleProduct';
 import Login from '../components/Login';
 import Register from '../components/Register';
 import PaymentSuccess from "../components/PaymentSuccess";
+import DashboardLayout from "../pages/dashboard/DashboardLayout";
 
 const router = createBrowserRouter([
     {
@@ -37,6 +38,28 @@ const router = createBrowserRouter([
         path: "/register",
         element: <Register />
     },
+
+     // dashboard routes 
+    {
+        path: "/dashboard",
+        element: <DashboardLayout />,
+        children: [
+            // User Routes 
+            {path: '', element: <div>User Dashboard</div> },
+            {path: 'orders', element: <div>User Order</div> },
+            {path: 'payments', element: <div>User Payments</div>},
+            {path: 'profile', element: <div>User Profile</div>},
+            {path: 'reviews', element: <div>User Reviews</div>},
+
+             // admin routes and include role field
+             {path: 'admin', element: <div>Admin Main</div> },
+             {path: 'add-product', element: <div>New Post</div> },
+             {path: 'manage-products', element: <div>Manage Post</div> },
+             {path: 'update-product/:id', element: <div>Update Post</div> },
+             {path: 'users', element: <div>All Users</div> },
+             {path: 'manage-orders', element: <div>Manage Order</div> },
+        ]
+     }
 ])
 
 export default router;
