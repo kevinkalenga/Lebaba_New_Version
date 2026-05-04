9fbba71ba7784102f406ce4274aa0f9c8f652d78
kevin kalenga <kevinkalenga10@gmail.com>
2026-05-05 00:13:59 +0200

    User Reviews Section

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -17,6 +17,7 @@ import UserDMain from "../pages/dashboard/user/dashboard/UserDMain";
 import UserOrders from "../pages/dashboard/user/UserOrders";
 import OrderDetails from "../pages/dashboard/user/OrderDetails";
 import UserPayments from "../pages/dashboard/user/UserPayments";
+import UserReviews from "../pages/dashboard/user/UserReviews";
 
 const router = createBrowserRouter([
     {
@@ -74,7 +75,7 @@ const router = createBrowserRouter([
             {path: 'orders', element: <UserOrders/> },
             {path: 'payments', element:  <UserPayments />},
             {path: 'profile', element: <div>User Profile</div>},
-            {path: 'reviews', element: <div>User Reviews</div>},
+            {path: 'reviews', element: <UserReviews/>},
 
              // admin routes and include role field
              {
