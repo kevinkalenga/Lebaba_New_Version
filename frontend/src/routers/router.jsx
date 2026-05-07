2419b2672bea747395a93babfccb14f93c3e26fe
kevin kalenga <kevinkalenga10@gmail.com>
2026-05-07 13:27:47 +0200

    User Role Management

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -23,7 +23,7 @@ import AdminDMain from "../pages/dashboard/admin/dashboard/AdminDMain";
 import AddProduct from "../pages/dashboard/admin/addProduct/AddProduct";
 import ManageProduct from "../pages/dashboard/admin/manageProduct/ManageProduct";
 import UpdateProduct from "../pages/dashboard/admin/manageProduct/UpdateProduct";
-
+import ManageUser from "../pages/dashboard/admin/users/ManageUser";
 
 const router = createBrowserRouter([
     {
@@ -102,7 +102,7 @@ const router = createBrowserRouter([
              },
              {
                 path: 'users', 
-                element: <PrivateRoute role="admin"><div>All Users</div></PrivateRoute> 
+                element: <PrivateRoute role="admin"><ManageUser/></PrivateRoute> 
              },
              {
                 path: 'manage-orders', 
