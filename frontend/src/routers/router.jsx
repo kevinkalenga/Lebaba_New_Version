5eafdf27a0a228eb9d77742b59db51d844c8a0bc
kevin kalenga <kevinkalenga10@gmail.com>
2026-05-07 10:12:26 +0200

    Admin Dashboard Design

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -19,6 +19,7 @@ import OrderDetails from "../pages/dashboard/user/OrderDetails";
 import UserPayments from "../pages/dashboard/user/UserPayments";
 import UserReviews from "../pages/dashboard/user/UserReviews";
 import UserProfile from "../pages/dashboard/user/UserProfile";
+import AdminDMain from "../pages/dashboard/admin/dashboard/AdminDMain/AdminDMain";
 
 const router = createBrowserRouter([
     {
@@ -81,7 +82,7 @@ const router = createBrowserRouter([
              // admin routes and include role field
              {
                 path: 'admin', 
-                element: <PrivateRoute role="admin"><div>Admin Main</div></PrivateRoute> 
+                element: <PrivateRoute role="admin"><AdminDMain/></PrivateRoute> 
              },
              {
                 path: 'add-product', 
